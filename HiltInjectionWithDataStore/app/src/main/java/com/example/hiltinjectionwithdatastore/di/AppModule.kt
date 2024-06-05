c52c71032ad535ebd513690d32476d14731ba740
CoreSinText <coresintext@gmail.com>
2024-06-05 14:52:30 +0700

    set up

--- HiltInjectionWithDataStore/app/src/main/java/com/example/hiltinjectionwithdatastore/di/AppModule.kt
+++ HiltInjectionWithDataStore/app/src/main/java/com/example/hiltinjectionwithdatastore/di/AppModule.kt
@@ -2,23 +2,43 @@ package com.example.hiltinjectionwithdatastore.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
+
+
+private const val USER_PREFERENCES = "user_preferences"
+private const val DATA_STORE_FILE_NAME = "user_prefs.pb"
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    private val Context.dataStore by preferencesDataStore("preferences")
+
 
     @Provides
     @Singleton
-    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
-        context.dataStore
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            migrations = listOf(SharedPreferencesMigration(context, USER_PREFERENCES)),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { context.preferencesDataStoreFile(DATA_STORE_FILE_NAME) }
+        )
+    }
 }
