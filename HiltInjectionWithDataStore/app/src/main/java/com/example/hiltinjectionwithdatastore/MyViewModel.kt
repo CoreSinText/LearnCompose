776af2adbaa27d64f578dfef0e52a158c8adbaba
CoreSinText <coresintext@gmail.com>
2024-06-05 13:11:15 +0700

    set up

--- HiltInjectionWithDataStore/app/src/main/java/com/example/hiltinjectionwithdatastore/MyViewModel.kt
+++ HiltInjectionWithDataStore/app/src/main/java/com/example/hiltinjectionwithdatastore/MyViewModel.kt
@@ -1,16 +1,17 @@
 package com.example.hiltinjectionwithdatastore
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.hiltinjectionwithdatastore.data.dataStore.UserDataStoreImpl
 import com.example.hiltinjectionwithdatastore.data.uiState.MyUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,7 +22,13 @@ class MyViewModel @Inject constructor(
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(MyUiState())
     val uiState: StateFlow<MyUiState> = _uiState.asStateFlow()
-    val nameStore:LiveData<String> = userDataStoreImpl.getName().asLiveData()
+    val nameStore: StateFlow<String> = userDataStoreImpl.getName().filter {
+        it.isEmpty()
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        "No Have Data"
+    )
 
 
     fun changeName(name: String) {
