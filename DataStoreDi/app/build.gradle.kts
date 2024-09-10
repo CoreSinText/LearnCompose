5ae1940fe838cb8b9f8d12878b0a8e62477f0d84
CoreSinText <coresintext@gmail.com>
2024-09-10 13:31:21 +0700

    update

--- DataStoreDi/app/build.gradle.kts
+++ DataStoreDi/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
@@ -66,4 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
 }
